66b83e2a5843f4af71defdfd00ec23622187bd9f
Christian Jiménez Roche <christianjimenezroche@gmail.com>
2026-08-24 10:56:27 +0200

    feat: New index component

--- js/loader.js
+++ js/loader.js
@@ -10,3 +10,4 @@ async function loadComponent(id, file) {
 
 loadComponent("header", "../components/header.html");
 loadComponent("footer", "../components/footer.html");
+loadComponent("index", "../components/index.html");
